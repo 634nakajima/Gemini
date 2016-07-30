73703d4c978b49b63ab143862b4811da0ed645ea
MusashiNakajima <634.nakajima@gmail.com>
2016-07-31 02:29:07 +0900

    update example

--- examples/SimpleIO/SimpleIO.ino
+++ examples/SimpleIO/SimpleIO.ino
@@ -3,13 +3,34 @@
 Gemini gemini;
 const char* ssid = "intermediakanno";
 const char* password = "kannolab";
+const int LED = 16;
+int cnt =0;
+int sensorID, outputID;
 
 void setup() {
-  // put your setup code here, to run once:
-    gemini.begin(ssid, password);
+  pinMode(LED, OUTPUT);
+  
+  //name input port with pin number or callback function. 
+  gemini.addInput("/LED", LED);
+  gemini.addInput("/inputFunc", &inputFunc);
+  
+  //name output port.
+  //This function sets outputID in the second argument with which you will send a value.
+  sensorID = gemini.addOutput("/SensorValue");
+  outputID = gemini.addOutput("/outputFunc");
+  
+  gemini.begin("SimpleIO", ssid, password);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  gemini.monitor();
+  delay(1);
+  if(cnt++%1000 == 0){
+    gemini.send(outputID, cnt);
+  }
 }
+
+void inputFunc(int val) {
+  //do something
+}
+
